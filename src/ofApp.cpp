97216945930acf04707cac61a29eceb0cfce9b24
MiguelLameiras <mlameirasmlameiras@gmail.com>
2022-02-19 18:26:34 +0000

    physics added

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,33 +1,59 @@
 #include "ofApp.h"
 
-body::body(int x, int y,int z, int hue)
+vector<body> bodies;
+int maxParticles;
+
+body::body(int x, int y, int z, double mass_, int hue)
 {
-    position = glm::vec3(x,y,z);
-    velocity = glm::vec3(0,0,0);
-    size = 4;
-    color.setHsb(hue,255,255,255);
+    position = glm::vec3(x, y, z);                                                             // Initial position
+    double init = 0.01;                                                                        // Range of possible initial velocities
+    velocity = glm::vec3(ofRandom(-init, init), ofRandom(-init, init), ofRandom(-init, init)); // Initial random velocity
+    mass = mass_;
+    size = mass / 10;
+    color.setHsb(hue, 255, 255, 255);
 }
 
-body::~body(){
-
+body::~body()
+{
 }
 
-void body::update(){
+void body::update(int body_num)
+{
+    acceleration_calculate(body_num);
+    velocity += acceleration;
     position += velocity;
-    double r = sqrt(position[0]*position[0] + position[1]*position[1] + position[2]*position[2]);//Distance to Mass
-    double abs_velocity = sqrt(10 / r);                                                          // Velocity modulus;
-    velocity[0] = abs_velocity;
-    velocity[1] = abs_velocity;
-    velocity[2] = abs_velocity;
 }
 
-void body::draw(){
+void body::draw()
+{
     ofSetColor(color);
     ofDrawSphere(position, size);
 }
 
+void body::acceleration_calculate(int body_num)
+{
+    double ax = 0;
+    double ay = 0;
+    double az = 0;
+
+    for (int i = 0; i < maxParticles; i++)
+    {
+        if (i != body_num)
+        {
+            double r = pow((sqrt(pow((position[0] - bodies[i].position[0]), 2) + pow((position[1] - bodies[i].position[1]), 2) + pow((position[2] - bodies[i].position[2]), 2))), 3);
+            ax -= 9.8 * bodies[i].mass * (position[0] - bodies[i].position[0]) / r;
+            ay -= 9.8 * bodies[i].mass * (position[1] - bodies[i].position[1]) / r;
+            az -= 9.8 * bodies[i].mass * (position[2] - bodies[i].position[2]) / r;
+        }
+    }
+    acceleration[0] = ax;
+    acceleration[1] = ay;
+    acceleration[2] = az;
+}
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup()
+{
     ofSetVerticalSync(true);
     ofSetBackgroundColor(0, 0, 0);
     hue = 0;
@@ -41,21 +67,25 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-    if(bodies.size() > maxParticles){
+void ofApp::update()
+{
+    if (bodies.size() > maxParticles)
+    {
         bodies.erase(bodies.begin());
     }
-    for (int i = 0; i < bodies.size();i++){
-        bodies[i].update();
+    for (int i = 0; i < bodies.size(); i++)
+    {
+        bodies[i].update(i);
     }
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
     cam.begin();
-    ofRotateDeg(180, 0, 0, 1);//Rotate the camera to a position
+    ofRotateDeg(180, 0, 0, 1); // Rotate the camera to a position
     light.enable();
-    ofDrawAxis(400);//Draw axis 
+    ofDrawAxis(400); // Draw axis
 
     for (int i = 0; i < bodies.size(); i++)
     {
@@ -67,64 +97,72 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-    switch (key) {
-        case 'f':
-        case 'F':
-            ofToggleFullscreen();
-            break;
-        case ' ':
-            int init = 5;
-            body newbody(ofRandom(-init, init), ofRandom(-init, init), ofRandom(-init, init), hue);
+void ofApp::keyPressed(int key)
+{
+    switch (key)
+    {
+    case 'f':
+    case 'F':
+        ofToggleFullscreen();
+        break;
+    case ' ':
+        int init = 200;
+        for (int i = 0; i < maxParticles; i++)
+        {
+            body newbody(ofRandom(-init, init), ofRandom(-init, init), ofRandom(-init, init), ofRandom(10, 60), hue);
             bodies.push_back(newbody);
-            break;
         }
+        break;
+    }
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key)
+{
     hue = ofRandom(255);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-
+void ofApp::mouseMoved(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
+void ofApp::mouseEntered(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
+void ofApp::mouseExited(int x, int y)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
+void ofApp::windowResized(int w, int h)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
+void ofApp::gotMessage(ofMessage msg)
+{
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo)
+{
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,26 +2,13 @@
 
 #include "ofMain.h"
 
-class body {
-public:
-	//Constructor and destructor
-	body(int x, int y, int z, int hue);//Initial coordinates and hue
-	~body();
-
-	void update();
-	void draw();
-
-	ofColor color;
-	float size;
-	glm::vec3 position, velocity; // 3D vectors for particles physics
-};
-
 class ofApp : public ofBaseApp
 {
 public:
 	void setup();
 	void update();
 	void draw();
+	
 
 	void keyPressed(int key);
 	void keyReleased(int key);
@@ -35,9 +22,26 @@ public:
 	void dragEvent(ofDragInfo dragInfo);
 	void gotMessage(ofMessage msg);
 
-	vector<body> bodies;
 	int hue;
 	ofEasyCam cam;
 	ofLight light;
-	int maxParticles;
 };
+
+class body
+{
+public:
+	//Constructor and destructor
+	body(int x, int y, int z, double mass_, int hue);//Initial coordinates and hue
+	~body();
+
+	void update(int body_num);
+	void draw();
+
+	void acceleration_calculate(int body_num);
+
+	ofColor color;
+	double size;
+	double mass;
+	glm::vec3 position, velocity, acceleration; // 3D vectors for particles physics
+};
+
